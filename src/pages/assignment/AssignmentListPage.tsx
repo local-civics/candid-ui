0d681c5a1255a348a3697b05dcc531f1d783f222
Percee Goings <percee@pghq.app>
2023-04-21 15:06:35 -0700

    Init assignments

--- src/pages/assignment/AssignmentListPage.tsx
+++ src/pages/assignment/AssignmentListPage.tsx
@@ -1,10 +1,43 @@
 import * as React        from 'react';
+import {
+    Container,
+    Title,
+    Button,
+    Divider,
+    Group, Flex, Stack,
+    Tooltip,
+    useMantineTheme, Card, Text, Menu, ActionIcon, createStyles, rem,
+} from '@mantine/core'
+import {
+    IconPlus,
+    IconLayoutKanban, IconDotsVertical, IconAbc, IconArchive,
+    IconPresentationAnalytics, IconUserEdit, IconFocusCentered,
+}             from '@tabler/icons-react'
+
+const useStyles = createStyles((theme) => {
+    return {
+        card: {
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+            height: 'max-content',
+            width: '20rem',
+        },
+
+        section: {
+            borderBottom: `${rem(1)} solid ${
+                theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
+            }`,
+            paddingLeft: theme.spacing.md,
+            paddingRight: theme.spacing.md,
+            paddingBottom: theme.spacing.md,
+        },
+    }
+})
 
 /**
  * AssignmentListPageProps
  */
 export type AssignmentListPageProps = {
-
+    assignments: {title: string, status: "open" | "assigned to me" | "archived"}[]
 }
 
 /**
@@ -13,12 +46,138 @@ export type AssignmentListPageProps = {
  * @constructor
  */
 export function AssignmentListPage(props: AssignmentListPageProps){
+    const theme = useMantineTheme()
+    const {classes} = useStyles()
+    const openAssignments = props.assignments.filter(a => a.status === 'open').map(a => {
+        return <Card withBorder radius="md" p="md" className={classes.card}>
+            <Card.Section
+                sx={(theme) => ({
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundImage: theme.fn.gradient({ from: 'blue.8', to: 'blue.7', deg: 20 }),
+                    color: theme.white,
+                })}
+                px={20} pt={20} pb={50}>
+                <Group grow align="start" position="apart" spacing={0}>
+                    <Text fz="lg" maw="initial" fw={500}>
+                        {a.title}
+                    </Text>
+                    <Menu
+                        transitionProps={{ transition: 'pop' }}
+                        withArrow
+                        position="bottom-end"
+                        withinPortal
+                    >
+                        <Menu.Target>
+                            <ActionIcon variant="transparent" maw="max-content">
+                                <IconDotsVertical color="white" size="1rem" stroke={1.5} />
+                            </ActionIcon>
+                        </Menu.Target>
+                        <Menu.Dropdown>
+                            <Menu.Item icon={<IconAbc size="1rem" stroke={1.5} />}>Rename</Menu.Item>
+                            <Menu.Item color="red" icon={<IconArchive size="1rem" stroke={1.5} />}>Archive</Menu.Item>
+                        </Menu.Dropdown>
+                    </Menu>
+                </Group>
+            </Card.Section>
+
+            <Card.Section className={classes.section} mt="md">
+                <Group spacing={20}>
+                    <Tooltip label="Open assignment">
+                        <ActionIcon color="blue"><IconFocusCentered /></ActionIcon>
+                    </Tooltip>
+                    <Tooltip label="View progress">
+                        <ActionIcon color="blue"><IconPresentationAnalytics /></ActionIcon>
+                    </Tooltip>
+                    <Tooltip label="Edit assignees">
+                        <ActionIcon color="blue"><IconUserEdit /></ActionIcon>
+                    </Tooltip>
+                </Group>
+            </Card.Section>
+        </Card>
+    })
+
+    const assignedToMe = props.assignments.filter(a => a.status === 'assigned to me').map(a => {
+        return <Card withBorder radius="md" p="md" className={classes.card}>
+            <Card.Section
+                sx={(theme) => ({
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundImage: theme.fn.gradient({ from: 'blue.8', to: 'blue.7', deg: 20 }),
+                    color: theme.white,
+                })}
+                px={20} pt={20} pb={50}>
+                <Group grow align="start" position="apart" spacing={0}>
+                    <Text fz="lg" maw="initial" fw={500}>
+                        {a.title}
+                    </Text>
+                </Group>
+            </Card.Section>
+
+            <Card.Section className={classes.section} mt="md">
+                <Group spacing={20}>
+                    <Tooltip label="Open assignment">
+                        <ActionIcon color="blue"><IconFocusCentered /></ActionIcon>
+                    </Tooltip>
+                </Group>
+            </Card.Section>
+        </Card>
+    })
+
+    const archivedAssignments = props.assignments.filter(a => a.status === 'archived').map(a => {
+        return <Card withBorder radius="md" p="md" className={classes.card}>
+            <Card.Section
+                sx={(theme) => ({
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundImage: theme.fn.gradient({ from: 'blue.8', to: 'blue.7', deg: 20 }),
+                    color: theme.white,
+                })}
+                px={20} pt={20} pb={50}>
+                <Group grow align="start" position="apart" spacing={0}>
+                    <Text fz="lg" maw="initial" fw={500}>
+                        {a.title}
+                    </Text>
+                </Group>
+            </Card.Section>
+
+            <Card.Section className={classes.section} mt="md">
+                <Group spacing={20}>
+                    <Tooltip label="Open assignment">
+                        <ActionIcon color="blue"><IconFocusCentered /></ActionIcon>
+                    </Tooltip>
+                    <Tooltip label="View progress">
+                        <ActionIcon color="blue"><IconPresentationAnalytics /></ActionIcon>
+                    </Tooltip>
+                </Group>
+            </Card.Section>
+        </Card>
+    })
+
     // todo: new assignment action
     // todo: open/check progress assignment action
     // todo: edit assignees action
     // todo: rename assignment action
     // todo: delete assignment action
-    // todo: filter by assigned by
-    // todo: filter by post name
-    return null
+    return <Container size="lg" pb="xl">
+        <Stack spacing={10}>
+            <Group>
+                <IconLayoutKanban color={theme.colors.dark[4]}/>
+                <Title color="dark.4">Assignments</Title>
+            </Group>
+            <Divider/>
+            <Button maw="max-content" variant="subtle" leftIcon={<IconPlus />}>New assignment</Button>
+            <Flex mt={10} gap={15}>
+                {openAssignments}
+            </Flex>
+            <Title mt={40} underline size="sm" color="dark.4">Assigned to me</Title>
+            <Flex mt={10} gap={15}>
+                {assignedToMe}
+            </Flex>
+            <Title mt={40} underline size="sm" color="dark.4">Archived</Title>
+            <Flex mt={10} gap={15}>
+                {archivedAssignments}
+            </Flex>
+        </Stack>
+    </Container>
 }
--- stories/AssignmentListPage.stories.js
+++ stories/AssignmentListPage.stories.js
@@ -3,6 +3,33 @@ import {MemoryRouter} from "react-router-dom";
 import {AppLayout} from "../src/layouts/app/AppLayout";
 import { AssignmentListPage } from '../src/pages/assignment/AssignmentListPage';
 
+const MOCK_DATA = [
+  {
+    title: 'Assignment #1',
+    status: 'open',
+  },
+  {
+    title: 'Assignment #2',
+    status: 'open',
+  },
+  {
+    title: 'Assignment #3',
+    status: 'assigned to me',
+  },
+  {
+    title: 'Assignment #4',
+    status: 'assigned to me',
+  },
+  {
+    title: 'Assignment #5',
+    status: 'archived',
+  },
+  {
+    title: 'Assignment #6',
+    status: 'archived',
+  },
+]
+
 export default {
   title: 'AssignmentListPage',
   component: AssignmentListPage,
@@ -18,13 +45,34 @@ const Template = {
   render: (args) => <div className="h-full w-full overscroll-none font-proxima">
     <MemoryRouter>
       <AppLayout
-          page=<AssignmentListPage {...args}
-          />
+          page=<AssignmentListPage {...args}/>
       />
     </MemoryRouter>
   </div>,
 }
 
-export const Default = {
+export const NoAssignments = {
   ...Template,
+  args: {
+    assignments: [],
+  }
+};
+
+export const WithAssignments = {
+  ...Template,
+  args: {
+    assignments: MOCK_DATA,
+  }
+};
+
+export const AssignedToMe = {
+  ...Template,
+  args: {
+    assignments: MOCK_DATA.filter(a => a.status === 'assigned to me'),
+  }
+};
+
+export const Archived = {
+  ...Template,
+  assignments: MOCK_DATA.filter(a => a.status === 'archived'),
 };
