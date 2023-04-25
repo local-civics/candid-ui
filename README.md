805bd1f816457dc999e1ad839bd6e5bb325d57c3
perceeg <48067076+perceeg@users.noreply.github.com>
2023-04-25 20:56:03 +0000

    Deploying to gh-pages from  @ ae43bf263322b7a45ec2dbf852b21ce60c5d09d5 🚀

--- README.md
+++
@@ -1,2 +0,0 @@
-# connect-ui
-Next-gen component library for the Local Civics Connect platform
--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-
-const { defaults } = require("jest-config");
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
-};
